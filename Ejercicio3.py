54d5e5b190cdd79bdefc59099cb70464831a8dac
karmapolice01 <karmapolice011@gmail.com>
2016-11-24 17:10:18 -0600

    Add files via upload

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+#Marco Antonio Moreno Miranda#Marco Antonio Moreno Miranda
+#Jueves, 24 de noviembre del 2016
+#ejercicio 3
+pi=3.1416
+
+radio= int (input ("Inserte radio "))
+
+area=pi*radio*radio
+volumen= (4*pi*radio*radio*radio)/3
+print "area del circulo", area
+print "Volumen de esfera", volumen
